376104bbe27c8057b0e791098db6f5b3d7ff6f56
HaiyangFu <fuhy5402@163.com>
2020-05-09 02:33:15 +0800

    解析学校网页通告

---
+++ app/src/main/java/com/swufe/firstapp/SecondActivity.java
@@ -0,0 +1,45 @@
+package com.swufe.firstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+    TextView score;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        score=(TextView)findViewById(R.id.score);
+
+
+    }
+    public void butAdd1(View but){
+        showScore(1);
+
+    }
+    public void butAdd2(View but){
+        showScore(2);
+    }
+    public void butAdd3(View but){
+        showScore(3);
+
+    }
+    public void butreset(View but){
+        score.setText("0");
+
+    }
+    private void showScore(int inc){
+        String oldScore= (String) score.getText();
+        int newScore=Integer.parseInt(oldScore)+inc;
+        score.setText(""+newScore);
+
+
+
+
+
+    }
+}
